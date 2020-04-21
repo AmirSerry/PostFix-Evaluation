4b87432fa66fcc1c761e830f865db067c4056680
Amir Serry <e.amirserry@gmail.com>
2020-04-21 15:34:59 +0200

    Adding method for converting to infix format
    add convert to infix method that take the equation and convert into infix to be ready for the postfix evaluation using the stack
    
    #infix

--- src/com/amirserry/postfix_evaluation/EvaluateExpersion.java
+++ src/com/amirserry/postfix_evaluation/EvaluateExpersion.java
@@ -3,6 +3,51 @@ package com.amirserry.postfix_evaluation;
 public class EvaluateExpersion {
 
 	
+	private Stack opStack= new Stack();
+	
+	private String convertToPostFix(String infix) {
+		String[] items= infix.split(" ");
+		String infixString="";
+		for(String item:items) {
+			if(isNumeric(item)) {
+				infixString=infixString + " " + item;
+			}
+			else {
+				if(opStack.isEmpty()) {
+					opStack.push(item);
+				}
+				else {
+					String op1 = opStack.pop();
+					String op2 = item;
+					Integer opResult=checkPriorization(op1,op2);
+					if(opResult==1)
+					{
+						opStack.push(op1);
+						opStack.push(op2);
+					}
+					else {
+						infixString=infixString + " " + op1;
+						while(!opStack.isEmpty()) {
+							infixString=infixString + " " + opStack.pop();
+						}
+						opStack.push(op2);
+						
+						
+					}
+					
+				}
+				
+			}
+			
+		}
+		while(!opStack.isEmpty()) {
+			infixString=infixString + " " + opStack.pop();
+		}
+
+		System.out.println("PostFix is:" + infixString);
+		return infixString;
+	}
+	
 private double calaucate(double firstItem,double secondItem,String operante) {
 		
 		if(operante.equals("*"))
